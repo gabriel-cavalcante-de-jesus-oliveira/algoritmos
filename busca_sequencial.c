f192523eb174e9fa6464cb4c820072dde48c084b
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-03-30 12:44:49 -0300

    Implementei procedimento preencheRegistros

--- busca_sequencial.c
+++ busca_sequencial.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
-#define TAM_NOME = 51
-#define TAM_MATRICULA 21
+#include <string.h>
+#define TAM_NOME 51
 #define TAM_CURSO 31
-#define QTD_ALUNOS 40
-typedef int TipoChave;
+#define QTD_ALUNOS 3
+typedef long TipoChave;
 typedef struct{
     char nome[TAM_NOME];
-    char matricula[TAM_MATRICULA];
+    TipoChave matricula;
     char curso[TAM_CURSO];
     int turma;
 } Aluno;
-Aluno a[QTD_ALUNOS], *ptr_aluno = &a[0];
+Aluno a[QTD_ALUNOS], *ptrAluno = &a[0];
+void preencheRegistros(){
+    strcpy(ptrAluno->nome, "Aluno 1");
+    ptrAluno->matricula = 1111111;
+    strcpy(ptrAluno->curso, "Curso 1");
+    ptrAluno->turma = 1711;
+
+    strcpy((ptrAluno + 1)->nome, "Aluno 2");
+    (ptrAluno + 1)->matricula = 2222222;
+    strcpy((ptrAluno + 1)->curso, "Curso 2");
+    (ptrAluno + 1)->turma = 1712;
+
+    strcpy((ptrAluno + 2)->nome, "Aluno 3");
+    (ptrAluno + 2)->matricula = 3333333;
+    strcpy((ptrAluno + 2)->curso, "Curso 3");
+    (ptrAluno + 2)->turma = 1713;
+}
+int buscaSequencial(TipoChave chave){
+    ;
+}
 int main(){
+    TipoChave chave;
+    preencheRegistros();
+    printf("Matrícula do aluno consultado: ");
+    scanf("%ld", &chave); // especificador de formato para "signed long int"
+    buscaSequencial(chave);
     return 0;
 }
