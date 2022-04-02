7af51d544db1590b2c78e3c91c5e6dc59a6d1fc7
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-04-02 11:18:30 -0300

    Implementado procedimento que preenche registros

--- busca_binaria.c
+++ busca_binaria.c
@@ -11,6 +11,22 @@ typedef struct{
     int turma;
 } Aluno;
 Aluno a[QTD_ALUNOS], *ptrAluno = &a[0];
+void preencheRegistros(){
+    strcpy(ptrAluno->nome, "Aluno 1");
+    ptrAluno->matricula = 1111111;
+    strcpy(ptrAluno->curso, "Curso 1");
+    ptrAluno->turma = 1711;
+
+    strcpy((ptrAluno + 1)->nome, "Aluno 2");
+    (ptrAluno + 1)->matricula = 2222222;
+    strcpy((ptrAluno + 1)->curso, "Curso 2");
+    (ptrAluno + 1)->turma = 1712;
+
+    strcpy((ptrAluno + 2)->nome, "Aluno 3");
+    (ptrAluno + 2)->matricula = 3333333;
+    strcpy((ptrAluno + 2)->curso, "Curso 3");
+    (ptrAluno + 2)->turma = 1713;
+}
 int main(){
     return 0;
 }
