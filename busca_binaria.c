a33d35c1ea6ec9808783d1f08d89a071dd2b7e64
gabriel-cavalcante-de-jesus-oliveira <gabriel.lcifba@gmail.com>
2022-04-02 12:01:16 -0300

    Implementado função de busca e saída de dados

--- busca_binaria.c
+++ busca_binaria.c
@@ -27,6 +27,39 @@ void preencheRegistros(){
     strcpy((ptrAluno + 2)->curso, "Curso 3");
     (ptrAluno + 2)->turma = 1713;
 }
+int buscaBinaria(TipoChave chave){
+    int i, i_chave = -1, inicio = 0, fim = QTD_ALUNOS - 1, meio = (inicio + fim) / 2; //início, meio e fim do arranjo
+    do{
+        if((ptrAluno + meio)->matricula == chave){
+            i_chave = meio;
+            break;
+        }
+        else{
+            if((ptrAluno + meio)->matricula > chave){
+                fim = meio - 1;
+            } else{
+                inicio = meio + 1;
+            }
+        }
+        meio = (inicio + fim) / 2;
+    } while(meio <= fim);
+    return i_chave;
+}
 int main(){
+    int i_chave;
+    TipoChave chave;
+    preencheRegistros();
+    printf("Matrícula do aluno consultado: ");
+    scanf("%ld", &chave); // especificador de formato para "signed long int"
+    i_chave = buscaBinaria(chave);
+    if(i_chave == -1){
+        puts("Aluno não encontrado!");
+    } else{
+    puts("Aluno consultado");
+    printf("Nome: %s\n", (ptrAluno + i_chave)->nome);
+    printf("Matrícula: %ld\n", (ptrAluno + i_chave)->matricula);
+    printf("Curso: %s\n", (ptrAluno + i_chave)->curso);
+    printf("Turma: %d\n", (ptrAluno + i_chave)->turma);
+    }
     return 0;
 }
